88798f60c1e87ca049730016cbef6e7ecd93bb89
Jad Abi Abdallah <jad.abi.abdallah@gmail.com>
2013-06-11 20:49:27 +0200

    Suggestion + Detail Activities

--- TCApp/trunk/src/TCApp/ViewController.m
+++ TCApp/trunk/src/TCApp/ViewController.m
@@ -9,23 +9,74 @@
 //Controllers
 #import "ViewController.h"
 #import "TCInterestsPickerViewController.h"
+#import "TCActivity.h"
+#import "TCTchillrServerClient.h"
+#import "TCActivityTableViewCell.h"
 
 @interface ViewController ()
 
 @property (nonatomic, assign, getter = isInterestsPickerHasBeenShown) BOOL interestsPickerHasBeenShown;
+@property (nonatomic, retain) IBOutlet UITableView * tableView;
+@property (nonatomic, retain) NSArray * activities;
+
 @end
 
 @implementation ViewController
 
+@synthesize tableView = _tableView;
+@synthesize activities = _activities;
+- (NSArray *)activities {
+    if (_activities == nil) {
+        _activities = [[NSArray alloc] init];
+    }
+    return _activities;
+}
+
+#pragma mark Lifecycle
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [[TCTchillrServerClient sharedTchillrServerClient] startUserActivitiesRequestWithSuccess:^(NSArray *activitiesArray) {
+        self.activities = activitiesArray;
+        [self.tableView reloadData];
+    } failure:^(NSError *error) {
+        NSLog(@"%@",[error description]);
+    } offset:0 limit:1000];
+    [self setTitle:@"Suggestions"];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     if(!self.isInterestsPickerHasBeenShown) {
         [self performSegueWithIdentifier:NSStringFromClass([TCInterestsPickerViewController class]) sender:nil];
     }
+}
+
+#pragma mark Activity Access
+
+- (NSUInteger)numberOfActivities {
+    return [self.activities count];
+}
+
+- (TCActivity *)activityAtIndex:(NSUInteger)index {
+    return (TCActivity*)[self.activities objectAtIndex:index];
+}
+
+#pragma mark - UITableViewDataSource / Delegate methods
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self numberOfActivities];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TCActivityTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ActivityTableViewCellIdentifier"];
+    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+    TCActivity * activity = [self activityAtIndex:indexPath.row];
+    [cell.activityNameLabel setText:activity.name];
+    [cell.activityShortDescriptionLabel setText:activity.shortDescription];
+    [cell.activityNextOccurenceLabel setText:activity.formattedOccurence];
+    BOOL activityHasTags = [activity hasTags];
+    [cell.activityContextualTags setHidden:!activityHasTags];
+    [cell.activityContextualTags setText:[activity formattedContextualTags]];
+    return cell;
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
