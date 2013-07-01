0f2cc05f3af8e87c655d6ac1bec09c5f8ad6967a
Zedenem <contact@zedenem.com>
2013-07-01 16:37:24 +0200

    GPX File added to center map on Paris

--- Tchillr/trunk/src/Tchillr/TCRouteViewController.m
+++ Tchillr/trunk/src/Tchillr/TCRouteViewController.m
@@ -14,14 +14,17 @@
 // Model
 #import "TCActivity.h"
 
-@interface TCRouteViewController () <MKMapViewDelegate>
-
-#pragma mark Map Management
-@property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
+@interface TCRouteViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
 
 #pragma mark Back Navigation
 - (IBAction)popViewController:(id)sender;
+
+#pragma mark User Location
+@property (strong, nonatomic) CLLocationManager *userLocationManager;
+- (void)startUpdatingUserLocation;
+
+#pragma mark Map Management
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -30,6 +33,7 @@
 #pragma mark View Lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
+	[self startUpdatingUserLocation];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -38,6 +42,30 @@
 #pragma mark Back Navigation
 - (IBAction)popViewController:(id)sender {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+#pragma mark User Location
+- (CLLocationManager *)userLocationManager {
+	if (!_userLocationManager) {
+		_userLocationManager = [[CLLocationManager alloc] init];
+		[_userLocationManager setDesiredAccuracy:kCLLocationAccuracyBestForNavigation];
+		[_userLocationManager setDistanceFilter:2.0];
+		[_userLocationManager setDelegate:self];
+	}
+	return _userLocationManager;
+}
+- (void)startUpdatingUserLocation {
+	if ([CLLocationManager locationServicesEnabled]) {
+		[self.userLocationManager startUpdatingLocation];
+	}
+}
+
+#pragma mark CLLocationManagerDelegate
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+	CLLocation *userLocation = [locations lastObject];
+	[self.mapView setCenterCoordinate:userLocation.coordinate animated:YES];
+	[self.mapView setRegion:MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 300.00, 300.00)];
 }
 
 @end
